7bb169c6498ba2a7a94d312448dbf617d26b12d1
Marcelo Rodrigues <marcelo.srrod@gmail.com>
2025-08-31 22:00:33 -0300

    Realizando o desafio de contar a quantidade de letras que o nome digitado tem

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+nome = input("Qual seu nome? ")
+
+contador = 0
+for i in nome:
+    contador = contador +1
+
+print("Seu nome tem " + str(contador) + " letras 'a'.")
+print("Olá, " + nome + "!")
