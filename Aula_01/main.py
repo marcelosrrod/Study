f94b481d2ca78b9cbec47c32b6663da9f8a7953a
Marcelo Rodrigues <marcelo.srrod@gmail.com>
2025-08-31 23:10:41 -0300

    Atualizando o arquivo main.py com os dois desafios da aula e o desafio final.

--- Aula_01/main.py
+++ Aula_01/main.py
@@ -1,3 +1,4 @@
+# Primeiro desafio da aula 01
 nome = input("Qual seu nome? ")
 
 contador = 0
@@ -6,3 +7,17 @@ for i in nome:
 
 print("Seu nome tem " + str(contador) + " letras.")
 print("Olá, " + nome + "!")
+
+# Segundo desafio da aula 01
+num2 = input("Digite outro número: ")
+num1 = input("Digite um número: ")
+print("A soma dos números é: " + str(int(num1) + int(num2)))
+
+# Desafio final da aula 01
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + "!")
+print("Por favor, responda as perguntas a seguir:")
+salario = int(input("Digite seu salário: "))
+bonus = float(input("Digite o bônus em porcentagem: "))
+salario_bonus = salario + 1000 + (salario * int(bonus) / 100)
+print(nome + ", seu salário com bônus é: " + str(salario_bonus))
