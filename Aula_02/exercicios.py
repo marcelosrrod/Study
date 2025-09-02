8e2beaefe29896381568a76e8b625621606cbf71
Marcelo Rodrigues <marcelo.srrod@gmail.com>
2025-09-01 22:07:40 -0300

    Resolvendo os exercícios do tipo inteiro da aula 02

---
+++ Aula_02/exercicios.py
@@ -0,0 +1,31 @@
+## Inteiros - Exercício 01
+n1_e1 = int(input("Digite o primeiro número: "))
+n2_e1 = int(input("Digite o segundo número: "))
+soma_e1 = n1_e1 + n2_e1
+print(f"A soma entre {n1_e1} e {n2_e1} é igual a {soma_e1}")
+
+## Inteiros - Exercício 02
+n1_e2 = input("Digite um número: ")
+resto_divisao_e2 = int(n1_e2) % 5
+print(f"O resto da divisão de {n1_e2} por 5 é igual a {resto_divisao_e2}")
+
+## Inteiros - Exercício 03
+n1_e3 = int(input("Digite um número: "))
+n2_e3 = int(input("Digite outro número: "))
+produto_e3 = n1_e3 * n2_e3
+print(f"O produto entre {n1_e3} e {n2_e3} é igual a {produto_e3}")
+
+## Inteiros - Exercício 04
+n1_e4 = int(input("Digite um número inteiro: "))
+n2_e4 = int(input("Digite outro número inteiro: "))
+divisao_e4 = n1_e4 // n2_e4
+print(f"A divisão inteira entre {n1_e4} e {n2_e4} é igual a {divisao_e4}")
+
+## Inteiros - Exercício 05
+n1_e5 = int(input("Digite um número: "))
+quad_n1_e5 = n1_e5 ** 2
+print(f"O quadrado de {n1_e5} é igual a {quad_n1_e5}")
+
+## --------------------------------------------------------------------------------##
+
+## Números de ponto flutuante - Exercício 01
